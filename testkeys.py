3e1d78081af3d75940b7a8b949feef95a02d31e0
abhinuvpitale <abhinuvpitale@gmail.com>
2018-06-30 13:45:07 +0000

    testkeys.py

--- testkeys.py
+++ testkeys.py
@@ -2,8 +2,11 @@ import urllib
 import os
 
 f = open('keys.dat')
-print f.readline()
-print f.readline()
-print f.readline()
+KEY = str(f.readline()).strip()
+TOKEN = str(f.readline()).strip()
+BOARD = str(f.readline()).strip()
+f.close()
 
+url = 'https://api.trello.com/1/boards/'+CARD+'/cards?key='+KEY+'&token='+TOKEN
+print urllib.urlopen()
 
